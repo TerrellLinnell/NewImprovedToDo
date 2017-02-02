060994242d22e9422427ebae1188383530bcadfa
terrell linnell <terrell.linnell@gmail.com>
2017-02-02 15:06:33 -0700

    created api endpoints for '/api/todos/:todo_id' - get, put, and delete

--- routes/toDoRoutes.js
+++ routes/toDoRoutes.js
@@ -28,7 +28,43 @@ router.route('/')//creates a new route at /api
   });
 
 
-// router.route('/api')
+router.route('/:todo_id')
+  .get(function(req, res){
+    ToDo.findById(req.params.todo_id, function(err, toDoData){
+      if (err) {
+        console.log("error finding toDo");
+      } else {
+        res.json(toDoData);
+      }
+    })
+  })
+  .put(function(req, res){
+    ToDo.findById(req.params.todo_id, function(err, toDoData){
+      if (err) {
+        console.log("error finding a specific ToDo!!!!");
+      } else {
+        toDoData.name = req.body.name ? req.body.name : toDoData.name;
+        toDoData.date = req.body.date ? req.body.date : toDoData.date;
+
+        toDoData.save(function(e, updatedToDo){
+          if (e) {
+            console.log("error with updated todo");
+          } else {
+            res.json(updatedToDo)
+          }
+        });
+      }
+    });
+  })
+  .delete(function(req,res){
+    ToDo.remove({ _id: req.params.todo_id}, function(err, t){
+      if (err) {
+        console.log(err, "Could not delete todo!!!");
+      } else {
+        res.json({message: "ToDo Deleted."})
+      }
+    });
+  });
 
 
 
